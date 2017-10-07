bd56bfa9b323c605781fa5629a9df0e01b6ec54c
Ryan <ryan.haynes@gmail.com>
2017-10-07 01:36:23 -0400

    Added script to filter subjects not included in the dataset

---
+++ routes/run/filterSubjectData.js
@@ -0,0 +1,33 @@
+
+var db = require('analysis/db');
+var fs = require('fusion/fs');
+var path = require('path');
+
+function getSubjectIndex() {
+  var index = {};
+  var filename = path.join(Const.sourcesDir,'catie2','GSA2016_125_025_21february2017');
+  var subjectList = fs.readFile(filename+'.subj','utf8').split('\n');
+  var subjects = [];
+  for (var k in subjectList) {
+    var parts = subjectList[k].split('=');
+    index[parts[0]] = true;
+  }
+
+  return index;
+}
+
+exports.main = function() {
+  var mid = 3;
+
+  var subjIndex = getSubjectIndex();
+
+  var model = db.models.get(mid);
+  var lines = fs.readFile(path.join(Const.modelsDir,model.name,model.name+'.csv'),'utf8').split('\n');
+  var res = [lines[0]];
+  for (var k=1;k<lines.length;k++) {
+    var row = lines[k].split(',');
+    if (subjIndex[row[0]]) res.push(lines[k]);
+  }
+  fs.writeFile(path.join(Const.modelsDir,model.name,model.name+'.csv.filtered'),res.join('\n'));
+}
+
