056e5eaababa56bdd8c2e0c1aa2a322910a442a0
Ryan <ryan.haynes@gmail.com>
2017-10-21 14:59:47 -0400

    Added catch for empty gene name in scheduling analysis

--- routes/run/scheduleAnalysis.js
+++ routes/run/scheduleAnalysis.js
@@ -4,14 +4,15 @@ var fs = require('fusion/fs');
 var path = require('path');
 
 exports.main = function() {
-  var model = 3;
+  if (process.argv.length == 4) {
+    var model = process.argv[3];
 
-
-  var filename = path.join(Const.sourcesDir,'catie2','GSA2016_125_025_21february2017');
-  var geneList = fs.readFile(filename+'.genes','utf8').split('\n');
-  for (var k in geneList) {
-    if (+k % 100 == 0) console.log(k+' of '+geneList.length);
-    db.worklist.addJob(geneList[k],model);
-  }
+    var filename = path.join(Const.sourcesDir,'catie2','GSA2016_125_025_21february2017');
+    var geneList = fs.readFile(filename+'.genes','utf8').split('\n');
+    for (var k in geneList) {
+      if (+k % 100 == 0) console.log(k+' of '+geneList.length);
+      if (geneList[k]) db.worklist.addJob(geneList[k],model);
+    }
+  } else console.log('Usage: scheduleAnalysis modelID');
 }
 
