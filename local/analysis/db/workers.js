eeeceae4832c42dca9967174793d2ec336561740
Ryan <ryan.haynes@gmail.com>
2017-10-26 00:37:32 -0400

    Fixed worker registration deadlocking

--- local/analysis/db/workers.js
+++ local/analysis/db/workers.js
@@ -7,11 +7,30 @@ exports.getWorkers = function() {
 
 exports.registerWorker = function(ip,id) {
   id = id || '';
-  var worker = sql.get('SELECT * FROM workers WHERE IP=? AND ID=?',arguments);
-  if (!worker) {
-    var res = sql.query('INSERT INTO workers(IP,ID) VALUES (:)',[ip,id]);
-    return res.insertId;
-  } else return worker.workerID;
+
+  var res = null;
+
+  sql.fork(function(sql) {
+
+    sql.query('LOCK TABLES workers WRITE');
+
+      var worker = sql.get('SELECT * FROM workers WHERE IP=? AND ID=?',[ip,id]);
+      if (!worker) {
+        console.log('About to register '+ip+'  '+id);
+        try {
+          sql.query('START TRANSACTION');
+          var insert = sql.query('INSERT INTO workers(IP,ID) VALUES (:)',[ip,id]);
+          res = insert.insertId
+          sql.query('COMMIT');
+        } catch(e) {
+          sql.query('ROLLBACK');
+        }
+      } else res = worker.workerID;
+
+    sql.query('UNLOCK TABLES');
+  });
+
+  return res;
 }
 
 exports.checkin = function(workerID) {
